0f5359736aa07823673494a52a98c8a08fff5cfa
ALittleDaisy <1152594956@qq.com>
2025-01-20 16:37:47 +0800

    修改注释罢了

--- callaapt.bat
+++ callaapt.bat
@@ -7,6 +7,7 @@
 %1\aapt dump configurations %2 > %3\configurations.txt
 %1\aapt list %2 > %3\list.txt
 
+@REM 用于跳转到目录
 explorer %3
 
 
@@ -16,11 +17,11 @@ goto annotation
 功能: 打印出apk中关于资源文件的信息
 
 dump strings: 打印APK中资源表字符串池的内容。
-dump badging: 打印APK中声明的应用程序的标签和图标。
+dump badging: apk的门面数据：app名称、图标、权限、sdk、启动activity等
 dump permissions: 打印APK中的权限。
 dump resources: 从APK打印资源表。（关键）
-dump configurations: 打印APK中的配置。
-list: 以列出 APK 文件中的所有文件和资源路径。（关键）
+dump configurations: 打印APK中的资源配置类别
+list: 以列出 APK 文件中的所有文件和资源路径，好像不包括string
 
 %1: 传aapt.exe/aapt2.exe所在路径
 %2: 传apk/jar/zip文件
